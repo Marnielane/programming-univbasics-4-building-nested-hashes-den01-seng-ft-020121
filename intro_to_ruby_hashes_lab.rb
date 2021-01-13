6ea1df9c3f3eb39ad716e1a09846313ca509101b
Marnie Robinson <marnielane@gmail.com>
2021-01-13 15:56:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,11 @@ def monopoly_with_second_tier
   # complexity.
   
     base_hash = {
-    railroads: {}
+    railroads: {
+      pieces: 4,
+      rent_in_dollars: {},
+      names: {}
+    }
     
   }
 
